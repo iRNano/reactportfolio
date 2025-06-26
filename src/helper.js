dc95f9b1914a4fd566b38bd7977a55fb133759bd
iRNano <valdepenas.adrian@gmail.com>
2025-06-26 23:19:50 +0800

    setup get cv from gdrive

---
+++ src/helper.js
@@ -0,0 +1,8 @@
+// https://drive.google.com/drive/folders/1rzyV4B-fES25JiDExWyCyjWUtU5tUt-S?usp=sharing
+// https://drive.google.com/drive/folders/1rzyV4B-fES25JiDExWyCyjWUtU5tUt-S?usp=sharing
+// AIzaSyDHPaEgbcZBxNauw2Py8NDxUQnJ0NzoR7U
+export const getCV = () => {
+    const FOLDER_ID = '1rzyV4B';
+    const API_KEY = "AIzaSyDHPaEgbcZBxNauw2Py8NDxUQnJ0NzoR7U";
+
+}
